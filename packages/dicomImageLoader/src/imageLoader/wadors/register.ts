589c25ddd8c627236bdbacb8993ca114e6e6452c
Amina Hasimbegovic <amina.hasimbegovic@dedalus.eu>
2024-10-31 11:13:32 +0100

    DVISA-2440: update version

--- packages/dicomImageLoader/src/imageLoader/wadors/register.ts
+++ packages/dicomImageLoader/src/imageLoader/wadors/register.ts
@@ -4,7 +4,7 @@ import { metaDataProvider } from '../wadouri/metaData/index';
 /**
  * Register wadors scheme and metadata provider.
  * NOTE: currently, wadouri loadImage and metadataProvider are used also for wadors
- * (see imports). For more information see:
+ * (see imports). For more information see: https://github.com/DedalusDIIT/cornerstone3D/pull/14
  */
 export default function (cornerstone) {
   cornerstone.registerImageLoader('wadors', loadImage);
